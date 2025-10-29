962a0d63933e9b489ac141d0a32d91e2f65a9fbe
Joel Fernandez <joelfernandez.relg@gmail.com>
2025-10-29 20:24:05 -0300

    Add ParamTypes to convert UserInfo parameters

---
+++ src/test/java/transformations/ParamTypes.java
@@ -0,0 +1,17 @@
+package transformations;
+
+import io.cucumber.java.ParameterType;
+import models.UserInfo;
+import utils.files.UserInfoReader;
+
+public class ParamTypes {
+
+    @ParameterType(".*")
+    public UserInfo userInfo(String userStatus) {
+        return switch (userStatus.toLowerCase()) {
+            case "unregistered" -> UserInfoReader.getUnregisteredUser();
+            case "registered" -> UserInfoReader.getRegisteredUser();
+            default -> throw new IllegalArgumentException("No UserInfo file for '%s' status".formatted(userStatus));
+        };
+    }
+}
